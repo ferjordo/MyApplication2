b4da3171d694be617c650cd882faec09756a4497
ferjordo <ferjordo2011@gmail.com>
2016-07-05 16:11:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nando/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nando.myapplication;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.*;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
